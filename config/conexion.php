01776ee16e428fc565a2c7c7cfe43c57510e8547
Juan <juan.jimenez@uc.cl>
2020-12-24 18:11:41 -0300

    added db connection

--- config/conexion.php
+++ config/conexion.php
@@ -1,12 +1,26 @@
 <?php
+   // LOCAL 
+  // try {
+  //  #Pide las variables para conectarse a la base de datos.
+  //  require('data.php');
+  //  # Se crea la instancia de PDO
+  //  $db_par = new PDO("pgsql:dbname=$databaseName_par;host=localhost;user=$user_par;password=$password_par");
+  //  $db_impar = new PDO("pgsql:dbname=$databaseName_impar;host=localhost;user=$user_impar;password=$password_impar");
+  // } catch (Exception $e) {
+  //  echo "No se pudo conectar a la base de datos: $e";
+  // }
+
+  // HEROKU
   try {
-   #Pide las variables para conectarse a la base de datos.
-   require('data.php');
-   # Se crea la instancia de PDO
-   $db_par = new PDO("pgsql:dbname=$databaseName_par;host=localhost;user=$user_par;password=$password_par");
-   $db_impar = new PDO("pgsql:dbname=$databaseName_impar;host=localhost;user=$user_impar;password=$password_impar");
-  } catch (Exception $e) {
-   echo "No se pudo conectar a la base de datos: $e";
-  }
+    #Pide las variables para conectarse a la base de datos.
+    require('data.php');
+    # Se crea la instancia de PDO
+    $db_par = new PDO("pgsql:dbname=$database1;host=$host1;port=$port1;user=$user1;password=$password1");
+    $db_impar = new PDO("pgsql:dbname=$database2;host=$host2;port=$port2;user=$user2;password=$password2");
+   } catch (Exception $e) {
+    echo "No se pudo conectar a la base de datos: $e";
+   }
+
+
 
 ?>
--- config/data.php
+++ config/data.php
@@ -1,8 +1,31 @@
 <?php
-  $user_par = 'postgres';
-  $password_par = 'postgres';
-  $databaseName_par = 'grupo90e3';
-  $user_impar = 'postgres';
-  $password_impar = 'postgres';
-  $databaseName_impar = 'grupo95e3';
+
+  // LOCAL
+  // $user_par = 'postgres';
+  // $password_par = 'postgres';
+  // $databaseName_par = 'grupo90e3';
+  // $user_impar = 'postgres';
+  // $password_impar = 'postgres';
+  // $databaseName_impar = 'grupo95e3';
+
+  // HEROKU DB1
+
+  $db1 = parse_url(getenv("DATABASE_URL"));
+  $host1 = $db1["host"];
+  $port1 = $db1["port"];
+  $user1 = $db1["user"];
+  $password1 = $db1["pass"];
+  $database1 = ltrim($db1["path"], "/");
+
+
+  // HEROKU DB2
+
+  $db2 = parse_url(getenv("HEROKU_POSTGRESQL_CHARCOAL_URL"));
+  $host2 = $db1["host"];
+  $port2 = $db1["port"];
+  $user2 = $db1["user"];
+  $password2 = $db1["pass"];
+  $database2 = ltrim($db1["path"], "/");
+
+
 ?>
